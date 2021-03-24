486e0a93ea07164d230e313a3d920c835e19f76b
TizzJohnson <tizznasty10@gmail.com>
2021-03-24 09:16:09 -0400

    Initial Commit

---
+++ Tairi XCode Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tairi XCode Assessment
+//
+//  Created by Tairi on 3/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
